4fc0e09ce6f1d5a63fbe50f55cc74545e3a0a7f3
Mitchell <mitchczaja@live.com>
2020-02-27 18:44:43 -0800

    Can now Like and retweet tweets

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -12,17 +12,36 @@ class TweetCellTableViewCell: UITableViewCell {
 
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
+    
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
+    @IBAction func favoriteTweet(_ sender: Any) {
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    var favorited:Bool = false
+    
+   // func setFavorite( isFavorite:Bool){
+    //    favorited = isFavorited
+    //    if (favorited) {
+    //        fav.Button.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+   //     }
+    //    else {
+    //        favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
 }
+
